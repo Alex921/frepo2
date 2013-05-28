d21e9a811f097a0f322426e1e28bb8d6a0a4fc5c
user1 <mail1@aol.com>
2013-05-28 14:20:56 -0700

    6

--- resumecuda.sh
+++ resumecuda.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
-
-
-FPATH="/home/ubuntu/"
-
-FILE_A="$FPATH/parameters.txt" 
-
-IP_PARAM=$(cat $FILE_A | cut -d ':' -f1)
-PASS_PARAM=$(cat $FILE_A | cut -d ':' -f2) 
+PARAM_FILE="/home/ubuntu/parameters.txt" 
+IP_PARAM=$(cat $PARAM_FILE | cut -d ':' -f1)
+PASS_PARAM=$(cat $PARAM_FILE | cut -d ':' -f2) 
 DATE=$(date)
 echo "$DATE	starting cudacminer.sh on startup" >> /home/ubuntu/log.txt 
-sh $FPATH/startcuda.sh $IP_PARAM $PASS_PARAM &
+bash home/ubuntu/frepo2/startcuda.sh $IP_PARAM $PASS_PARAM &
 exit 
--- startcuda.sh
+++ startcuda.sh
@@ -2,5 +2,11 @@
 
 export PATH=/usr/local/cuda-5.0/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-5.0/lib64:/usr/local/cuda-5.0/lib
-echo "$1:$2" > ~/parameters.txt
+
+if [ -a home/ubuntu/parameters.txt ] ; then
+	./cudaminer -l 56x8,56x8 -C 1,1 --url=http://${1}:8332 --userpass=growl.${2}:x
+	exit
+fi
+
+echo "$1:$2" > home/ubuntu/parameters.txt
 ./cudaminer -l 56x8,56x8 -C 1,1 --url=http://${1}:8332 --userpass=growl.${2}:x
