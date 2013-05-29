61322dc2d0af6625b6500744a4ffb7bb6cf6258c
user1 <mail1@aol.com>
2013-05-29 00:08:16 -0700

    7

--- startcuda.sh
+++ startcuda.sh
@@ -1,12 +1,10 @@
 #!/bin/bash 
+### script must be called with SCREEN
+
+if [ ! -f /home/ubuntu/parameters.txt ] ; then 
+	echo "${1};${2}" > /home/ubuntu/parameters.txt
+fi
 
 export PATH=/usr/local/cuda-5.0/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-5.0/lib64:/usr/local/cuda-5.0/lib
-
-if [ -a home/ubuntu/parameters.txt ] ; then
-	./cudaminer -l 56x8,56x8 -C 1,1 --url=http://${1}:8332 --userpass=growl.${2}:x
-	exit
-fi
-
-echo "$1:$2" > home/ubuntu/parameters.txt
-./cudaminer -l 56x8,56x8 -C 1,1 --url=http://${1}:8332 --userpass=growl.${2}:x
+home/ubuntu/cudaminer -l 56x8,56x8 -C 1,1 --url=http://${1}:8332 --userpass=growl.${2}:x
